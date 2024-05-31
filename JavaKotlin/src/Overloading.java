97412975f965d5ae37917cf83c9401c7b1ca2cdf
안승우 <rio319@naver.com>
2024-05-31 17:42:23 +0900

    자바 오버로딩 예제

---
+++ JavaKotlin/src/Overloading.java
@@ -0,0 +1,22 @@
+class Person {
+    public void eat(String food) {
+        System.out.println(STR."I eat \{food}");
+    }
+
+    // 매개변수의 개수 변화
+    public void eat(String food1, String food2) {
+        System.out.println(STR."I eat \{food1} and \{food2}");
+    }
+
+    // 매개변수의 타입 변화
+    public void eat(int gram) {
+        System.out.println(STR."I eat \{gram}g");
+    }
+
+    /*
+    메서드의 타입 변화만으론 오버로딩 성립이 안됨
+    public String eat(String food) {
+        return "I eat something";
+    }
+    */
+}
