dd6043b3599291fa04e300f4e2da4c611992f203
안승우 <rio319@naver.com>
2024-08-21 22:12:15 +0900

    ArrayList 인터페이스 구현

---
+++ DataStructure/src/MArrayList.java
@@ -0,0 +1,22 @@
+interface MList<E> {
+    boolean add(E e);
+    void add(int index, E e);
+
+    boolean remove(Object o);
+    E remove(int index);
+
+    E get(int index);
+    void set(int index, E e);
+
+    boolean contains(Object o);
+    int indexOf(Object o);
+    int lastIndexOf(Object o);
+
+    int size();
+    boolean isEmpty();
+
+    void clear();
+}
+
+
+
