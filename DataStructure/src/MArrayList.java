c8592fcc3846f4b7ae5ba4ae4ad4cf049f80f18d
안승우 <rio319@naver.com>
2024-08-21 22:51:13 +0900

    ArrayList 스켈레톤 코드 작성

--- DataStructure/src/MArrayList.java
+++ DataStructure/src/MArrayList.java
@@ -18,5 +18,181 @@ interface MList<E> {
     void clear();
 }
 
+public class MArrayList<E> implements MList<E> {
+
+    private static final int DEFAULT_CAPACITY = 10; //기본 용량
+    private static final Object[] EMPTY_ELEMENT_DATA = {}; // 빈 배열
+
+    private int size; // elementData 배열의 총 개수
+    Object[] elementData; // 자료를 담을 배열
+
+    /*
+    * 생성자 (초기 공간 할당 X)
+    * 디폴트 용량으로 초기화
+    * */
+    public MArrayList() {
+
+    }
+
+    /*
+    * 생성자 (초기 공간 할당 O)
+    * capacity 양수일 경우 해당 용량으로 배열 생성
+    * capacity 0일 경우 디폴트 용량으로 초기화
+    * capacity 음수일 경우 RuntimeException 의 IllegalAccessException 주기
+    * */
+    public MArrayList(int capacity) {
+
+    }
+
+    /*
+    * ArrayList 는 가변 배열이기 때문에 용량을 재설정함
+    * */
+    private void resize() {
+        int element_capacity = elementData.length;
+
+        /*
+        * 용량이 꽉찬 경우
+        * 1. 현재 용량의 두 배로 설정
+        * 2. 복사할 배열을 두 배로 설정한 용량만큼 설정하고, elementData 원소들을 전체 복사해넣고 반환
+        * */
+
+        /*
+        * 용량에 비해 데이터가 적은 경우
+        * 1. 개수가 용량 절반보다 적다면, 절반 용량을 만듦
+        * 2. 절반 용량과 디폴트 용량 중 큰 값을 용량으로 설정해 배열을 복사한 후 반환
+        * */
+
+        /*
+        * 들어있는 데이터가 하나도 없을 경우
+        * 1. 현재 빈 배열일 경우, 다시 기본 용량으로 초기화 후 반환
+        * */
+    }
+
+    /*
+    * 가장 마지막 부분에 데이터 삽입
+    * 먼저 resize()를 호출 후 마지막에 데이터 삽입
+    * */
+    @Override
+    public boolean add(E e) {
+        return true;
+    }
+
+    /*
+    * 배열 중간에 데이터를 삽입
+    * */
+    @Override
+    public void add(int index, E e) {
+        /*
+        * 인덱스가 음수이거나, 배열 크기를 벗어난 경우
+        * IndexOutOfBoundsException 발생
+        * */
+
+        /*
+        * 인덱스가 마지막 위치일 경우, 그냥 추가함
+        * 인덱스가 중간 위치일 경우
+        * 1. 먼저 resize() 호출
+        * 2. 맨 뒤에서부터 index 바로 뒤까지 한 칸씩 뒤로 밀어 빈 공간 만들기
+        * 3. index 에 해당 데이터 삽입함
+        * */
+    }
+
+    /*
+    * 특정 값 중 가장 처음 해당하는 요소를 제거함
+    * 1. 해당 값이 몇 번째 위치에 존재하는지 인덱스를 얻어옴
+    * 2. 인덱스가 -1이면 삭제하려는 값이 없는 것이니 그대로 메서드를 종료함
+    * 3. 인텍스를 찾았으면 remove(int index)로 넘겨 삭제함
+    * */
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    /*
+    * 특정 index 에 있는 값을 삭제함
+    * 1. 인덱스가 음수거나, size 보다 같거나 클 경우, IndexOutOFBoundsException 발생
+    * 2. 반환할 값 백업
+    * 3. 요소 null 처리로 제거 (명시적으로 null 처리를 해야 GC가 수거해감)
+    * 4. 삭제한 요소의 뒤에 있는 모든 요소들을 한 칸씩 당겨옴
+    * 5. 요소 제거했으니 size 감소함
+    * 6. capacity 가 남아돌 수 있으니 resize() 호출
+    * 7. 백업한 삭제된 요소를 반환
+    * */
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    /*
+    * index 에 저장된 요소를 가져옴
+    * 1. index 범위 체크
+    * 2. 요소값 반환 (형변환 필요)
+    * */
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    /*
+    * index 에 요소를 덮어씀
+    * 1. index 범위 체크
+    * 2. 요소값 덮어쓰기
+    * */
+    @Override
+    public void set(int index, E e) {
+
+    }
+
+    /*
+    * 찾고자 하는 요소가 포함되었다면 true, 아니라면 false 반환
+    * 요소의 인덱스값이 0보다 크면 true, 아니면 false
+    * */
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    /*
+    * 순차적으로 검색해서 해당 값이 있는 위치를 반환
+    * */
+    @Override
+    public int indexOf(Object o) {
+
+        // 매개변수가 null 일 경우 (ArrayList 는 null 도 저장 가능하기 때문에)
+
+        // 매개변수가 실질적인 값일 경우
+
+        // 찾는 값이 없는 경우 -1 반환
+        return 0;
+    }
+
+    /*
+    * 거꾸로 검색해서 해당 값이 있는 위치를 반환
+    * */
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /*
+    * 모든 요소 지우기
+    * 1. 기본 용량으로 초기화
+    * 2. size 도 초기화
+    * */
+    @Override
+    public void clear() {
+
+    }
+}
+
 
 
