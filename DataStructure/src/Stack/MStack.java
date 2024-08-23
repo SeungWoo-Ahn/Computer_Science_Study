036fc8eb51cfba12299bd85f8d66cc5c10b76582
rio319-an <78468001+rio319-an@users.noreply.github.com>
2024-08-23 18:35:13 +0900

    Stack 내부 구현

--- DataStructure/src/Stack/MStack.java
+++ DataStructure/src/Stack/MStack.java
@@ -1,5 +1,8 @@
 package Stack;
 
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
 public class MStack<E> implements StackInterface<E> {
     private static final int DEFAULT_CAPACITY = 10;
     private static final Object[] EMPTY_ARRAY = {};
@@ -8,11 +11,13 @@ public class MStack<E> implements StackInterface<E> {
     private int size;
 
     public MStack() {
-
+        this.array = new Object[DEFAULT_CAPACITY];
+        this.size = 0;
     }
 
     public MStack(int capacity) {
-
+        this.array = new Object[capacity];
+        this.size = 0;
     }
 
     /*
@@ -21,7 +26,20 @@ public class MStack<E> implements StackInterface<E> {
     * 3. 사이즈가 용량 절반 이하일 경우
     * */
     private void resize() {
-
+        if (Arrays.equals(array, EMPTY_ARRAY)) {
+            array = new Object[DEFAULT_CAPACITY];
+            return;
+        }
+        int array_length = array.length;
+        if (size == array_length) {
+            int doubled_length = array_length * 2;
+            array = Arrays.copyOf(array, doubled_length);
+            return;
+        }
+        if (size < array_length / 2) {
+            int half_length = array_length / 2;
+            array = Arrays.copyOf(array, Math.max(half_length, DEFAULT_CAPACITY));
+        }
     }
 
     /*
@@ -30,7 +48,12 @@ public class MStack<E> implements StackInterface<E> {
     * */
     @Override
     public E push(E e) {
-        return null;
+        if (size == array.length) {
+            resize();
+        }
+        array[size] = e;
+        size++;
+        return e;
     }
 
     /*
@@ -41,8 +64,16 @@ public class MStack<E> implements StackInterface<E> {
     * 5. resize()
     * */
     @Override
+    @SuppressWarnings("unchecked")
     public E pop() {
-        return null;
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        E data = (E) array[size - 1];
+        array[size - 1] = null;
+        size--;
+        resize();
+        return data;
     }
 
     /*
@@ -50,23 +81,41 @@ public class MStack<E> implements StackInterface<E> {
     * 2. 마지막 요소 반환
     * */
     @Override
+    @SuppressWarnings("unchecked")
     public E peek() {
-        return null;
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        return (E) array[size - 1];
     }
 
     /*
     * 스택의 Top 에서부터 찾아서 일치하는 첫 번째 요소가 Top 에서 몇 번째에 있는지 반환
+    * Top 부터 1로 셈
     * 1. 찾으려는 값이 null 일때
     * 2. 찾으려는 값이 null 이 아닐때
     * */
     @Override
     public int search(Object o) {
-        return 0;
+        if (o == null) {
+            for (int i = size - 1; i >= 0; i--) {
+                if (array[i] == null) {
+                    return size - i;
+                }
+            }
+        } else {
+            for (int i = size - 1; i >= 0; i--) {
+                if (array[i].equals(o)) {
+                    return size - i;
+                }
+            }
+        }
+        return -1;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     /*
@@ -76,11 +125,15 @@ public class MStack<E> implements StackInterface<E> {
     * */
     @Override
     public void clear() {
-
+        for (int i = 0; i < size; i++) {
+            array[i] = null;
+        }
+        size = 0;
+        resize();
     }
 
     @Override
     public boolean empty() {
-        return false;
+        return size == 0;
     }
 }
