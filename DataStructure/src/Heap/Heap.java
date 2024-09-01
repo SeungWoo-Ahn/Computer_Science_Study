876d79f32ebf03b069d21676dd3653640dffedc3
안승우 <rio319@naver.com>
2024-09-01 23:55:14 +0900

    Heap 내부 구현

--- DataStructure/src/Heap/Heap.java
+++ DataStructure/src/Heap/Heap.java
@@ -1,7 +1,7 @@
 package Heap;
 
 import java.util.Comparator;
-import java.util.Objects;
+import java.util.NoSuchElementException;
 
 public class Heap<E> {
     private final Comparator<? super E> comparator;
@@ -9,38 +9,47 @@ public class Heap<E> {
 
     private int size;
 
-    private Objects[] array;
+    private Object[] array;
 
     public Heap() {
-
+        this(null);
     }
 
     public Heap(Comparator<? super E> comparator) {
-
+        this.array = new Object[DEFAULT_CAPACITY];
+        this.comparator = comparator;
+        this.size = 0;
     }
 
     public Heap(int capacity) {
-
+        this(capacity, null);
     }
 
     public Heap(int capacity, Comparator<? super E> comparator) {
-
+        this.array = new Object[capacity];
+        this.comparator = comparator;
+        this.size = 0;
     }
 
     private int getParent(int index) {
-        return 0;
+        return index / 2;
     }
 
     private int getLeftChild(int index) {
-        return 0;
+        return index * 2;
     }
 
     private int getRightChild(int index) {
-        return 0;
+        return index * 2 + 1;
     }
 
     private void resize(int newCapacity) {
-
+        Object[] newArray = new Object[newCapacity];
+        for (int i = 1; i <= size; i++) {
+            newArray[i] = array[i];
+        }
+        this.array = null;
+        this.array = newArray;
     }
 
     /**
@@ -49,14 +58,22 @@ public class Heap<E> {
      * 3. 사이즈 증가
      */
     public void add(E e) {
-
+        if (size + 1 == array.length) {
+            resize(array.length * 2);
+        }
+        shiftDown(size + 1, e);
+        size++;
     }
 
     /**
      * 1. comparator 의 존재 여부로 나눠준다
      */
     private void siftUp(int index, E target) {
-
+        if (comparator != null) {
+            shiftUpComparator(index, target, comparator);
+        } else {
+            shiftUpComparable(index, target);
+        }
     }
 
     /**
@@ -66,16 +83,37 @@ public class Heap<E> {
      * 4. 타겟 노드의 값이 부모 노드보다 크면 반복문 종료
      * 5. 삽입될 위치에 타겟 노드의 값을 저장해준다.
      */
+    @SuppressWarnings("unchecked")
     private void shiftUpComparator(int index, E target, Comparator<? super E> comp) {
-
+        while (index > 1) {
+            int parentIdx = getParent(index);
+            E parentVal = (E) array[parentIdx];
+            if (comp.compare(target, parentVal) >= 0) {
+                break;
+            }
+            array[index] = parentVal;
+            index = parentIdx;
+        }
+        array[index] = target;
     }
 
     /**
      * 1. 타겟 노드가 비교될 수 있도록 comp 변수를 만든다
      * 2. shiftUpComparator 와 동일
      */
+    @SuppressWarnings("unchecked")
     private void shiftUpComparable(int index, E target) {
-
+        Comparable<? super E> comp = (Comparable<? super E>) target;
+        while (index > 1) {
+            int parentIdx = getParent(index);
+            E parentVal = (E) array[parentIdx];
+            if (comp.compareTo(parentVal) >= 0) {
+                break;
+            }
+            array[index] = parentVal;
+            index = parentIdx;
+        }
+        array[index] = comp;
     }
 
     /**
@@ -86,12 +124,29 @@ public class Heap<E> {
      * 5. 삭제할 노드의 인덱스와 이후 재배치 할 타겟 노드를 shiftDown 으로 넘겨준다
      * 6. 데이터 반환
      */
+    @SuppressWarnings("unchecked")
     public E remove() {
-        return null;
+        if (array[1] == null) {
+            throw new NoSuchElementException();
+        }
+        E data = (E) array[1];
+        E target;
+        if (size == 1) {
+            target = null;
+        } else {
+            target = (E) array[size];
+        }
+        array[size] = null;
+        shiftDown(1, target);
+        return data;
     }
 
     private void shiftDown(int index, E target) {
-
+        if (comparator != null) {
+            shiftDownComparator(index, target, comparator);
+        } else {
+            shiftUpComparable(index, target);
+        }
     }
 
     /**
@@ -108,23 +163,70 @@ public class Heap<E> {
      * 11. 재배치되는 위치에 타겟이 된 값을 넣어준다
      * 12. 용적 사이즈가 최소 용적보다 크면서 요소의 개수가 용적의 1/4일 때, 용적을 반으로 줄임
      */
+    @SuppressWarnings("unchecked")
     private void shiftDownComparator(int idx, E target, Comparator<? super E> comp) {
-
+        array[idx] = null;
+        size--;
+        int parent = idx;
+        int child;
+        while ((child = getLeftChild(parent)) <= size) {
+            int rightIdx = getRightChild(parent);
+            E childVal = (E) array[child];
+            if (rightIdx <= size && comp.compare(childVal, (E) array[rightIdx]) > 0) {
+                child = rightIdx;
+                childVal = (E) array[child];
+            }
+            if (comp.compare(target, childVal) <= 0) {
+                break;
+            }
+            array[parent] = childVal;
+            parent = child;
+        }
+        array[parent] = target;
+        if (array.length > DEFAULT_CAPACITY && size < array.length / 4) {
+            resize(Math.max(DEFAULT_CAPACITY, array.length / 2));
+        }
     }
 
+    @SuppressWarnings("unchecked")
     private void shitDownComparable(int index, E target) {
-
+        Comparable<? super E> comp = (Comparable<? super E>) target;
+        array[index] = null;
+        size--;
+        int parent = index;
+        int child;
+        while ((child = getLeftChild(parent)) <= size) {
+            int rightIdx = getRightChild(parent);
+            E childVal = (E) array[child];
+            if (rightIdx <= size && ((Comparable<? super E>) childVal).compareTo((E) array[rightIdx]) > 0) {
+                child = rightIdx;
+                childVal = (E) array[rightIdx];
+            }
+            if (comp.compareTo(childVal) <= 0) {
+                break;
+            }
+            array[parent] = childVal;
+            parent = child;
+        }
+        array[parent] = comp;
+        if (array.length > DEFAULT_CAPACITY && size < array.length / 4) {
+            resize(Math.max(DEFAULT_CAPACITY, array.length / 2));
+        }
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 
+    @SuppressWarnings("unchecked")
     public E peek() {
-        return null;
+        if (array[1] == null) {
+            throw new NoSuchElementException();
+        }
+        return (E) array[1];
     }
 
     public boolean insEmpty() {
-        return false;
+        return size == 0;
     }
 }
